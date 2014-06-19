55be240475d79cd0124809348d1a61ebd8fd08f6
Ben Brostoff <ben.brostoff@gmail.com>
2014-06-18 21:56:33 -0400

    Several methods added, including save

--- model.rb
+++ model.rb
@@ -12,20 +12,35 @@ class Student
   include StudentDatabase
 
   @@students = []
+  @@id_track = StudentDatabase.db.execute("SELECT max(id) from students")[0].values[0]
 
   attr_accessor :id, :first_name, :last_name, :email, :phone, :grade
 
   def initialize(params = {})
     @id = params["id"] #need to get to symbols ideally
-    @first_name = params["first_name"]
-    @last_name = params["last_name"]
-    @email = params["email"]
-    @phone = params["phone"]
-    @grade = params["grade"]
+    @first_name = params["first_name"] ||= "John"
+    @last_name = params["last_name"] ||= "Connor"
+    @email = params["email"] ||= "terminate@terminate.com"
+    @phone = params["phone"] ||= "123-546-789"
+    @grade = params["grade"] ||= "12"
   end
 
-  def save
+  def self.id_track
+    @@id_track
+  end
 
+  def save #is there a quicker way on first conditional?
+    if self.id <= @@id_track
+      StudentDatabase.db.execute("UPDATE students SET first_name = #{self.first_name} WHERE id = ?", self.id)
+    else
+      @@students << self
+      StudentDatabase.db.execute("INSERT INTO students VALUES (?,?,?,?,?,?)", self.all_attributes)
+    end
+
+  end
+
+  def all_attributes
+    all = [self.id, self.first_name, self.last_name, self.email, self.phone, self.grade]
   end
 
   def self.where(category, value)
@@ -43,5 +58,8 @@ class Student
 
 end
 
-p Student.where("id > ?", 25)
-
+# p Student.where("id > ?", 25)
+p Student.id_track
+john = Student.new ({"id" => 29})
+p john
+john.save
